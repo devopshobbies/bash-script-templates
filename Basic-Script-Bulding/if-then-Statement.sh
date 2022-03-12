8a944a1d69e6bfdc18f0761274002f6d6877f1f2
AlirezaBahadoriZadeh <101427194+AlirezaBahadoriZadeh2002@users.noreply.github.com>
2022-03-12 13:28:51 +0330

    Update if-then-Statement.sh

--- Basic-Script-Bulding/if-then-Statement.sh
+++ Basic-Script-Bulding/if-then-Statement.sh
@@ -12,5 +12,16 @@ testuser=rich
 if grep $testuser /etc/passwd
 then
 echo The bash files for user $testuser are:
-ls -a / home/$testuser/.b*
+ls -a /home/$testuser
 fi
+
+
+
+# To Answer :
+# useradd rich
+# passwd rich
+# change to root and :
+# cd /home/rich
+# touch test.txt
+# Run the Bashscript file
+

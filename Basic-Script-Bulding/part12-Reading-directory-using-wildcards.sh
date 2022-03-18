14e17526c0684fc6a1fd31e67ffecd69679b0d7f
AlirezaBahadoriZadeh <101427194+AlirezaBahadoriZadeh2002@users.noreply.github.com>
2022-03-18 11:02:50 +0330

    Create part12-Reading-directory-using-wildcards.sh

---
+++ Basic-Script-Bulding/part12-Reading-directory-using-wildcards.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# iterate through all the files in a directory
+
+for file in /home/rich/*
+do
+if [ -d "$file" ]
+then
+echo "$file is a directory"
+elif [ -f "$file" ]
+Then
+echo "$file is a file"
+fi
+done
+
+# For Answer :
+# useradd rich
+# In /home/rich/ :
+# touch test

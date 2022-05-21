3f977ea25857e1349905616edb47afa2be9400c3
Javid <80752212+MohammadrezaJavid@users.noreply.github.com>
2022-05-21 14:00:44 -0400

    Update part13-The-C-Style-for-Command.sh
    In this example, an input number between 5 and 9 is received from the loop and a rhombus is displayed at the output.

--- Basic-Script-Bulding/part13-The-C-Style-for-Command.sh
+++ Basic-Script-Bulding/part13-The-C-Style-for-Command.sh
@@ -9,3 +9,48 @@ for (( i=1; i ‹= 10; i++ ))
 do
 echo "The next number is $i"
 done
+
+# An example of for loops (C-Style)
+
+let TMP=0
+
+echo -n "Enter Number between (5 to 9): "
+read TMP
+
+if ! [ $TMP -ge 5 -a $TMP -le 9 ]; then
+        echo "pelese enter number between 5 and 9, Try Again."
+        exit 1
+fi
+
+
+clear
+
+for ((i = 1; i <= TMP; i++)); do
+
+        for ((s = TMP; s >= i; s--)); do
+                echo -n " "
+        done
+
+        for ((j = 1; j <= i; j++)); do
+                echo -n " ."
+        done
+
+        echo ""
+
+done
+
+################### Second stage ######################
+
+for ((i = TMP; i >= 1; i--)); do
+
+        for ((s = i; s <= TMP; s++)); do
+                echo -n " "
+        done
+
+        for ((j = 1; j <= i; j++)); do
+                echo -n " ."
+        done
+
+        echo ""
+
+done
